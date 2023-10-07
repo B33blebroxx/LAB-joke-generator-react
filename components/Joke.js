9032434c4148005f82cd645ba8c83ed75049a7df
Brandon Schnurbusch <b33blbroxx@gmail.com>
2023-10-07 12:01:48 -0500

    Jokes Generating

---
+++ components/Joke.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Joke({ joker, btnText }) {
+  return (
+    <>
+      <h1>{joker.setup}</h1>
+      <h2>{btnText !== 'Get A Punchline' ? joker.delivery : ''}</h2>
+    </>
+  );
+}
+
+Joke.propTypes = {
+  joker: PropTypes.shape({
+    setup: PropTypes.string,
+    delivery: PropTypes.string,
+  }).isRequired,
+  btnText: PropTypes.string.isRequired,
+};
